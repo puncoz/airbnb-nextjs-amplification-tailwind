89ea17391645bde441e8d7ca148478db8b643071
Puncoz Nepal <puncoz138@gmail.com>
2023-08-18 21:34:28 +0900

    frontend scaffold

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "airbnb-gradient": "linear-gradient(to right,#E61E4D 0%,#E31C5F 50%,#D70466 100%)",
+            },
+
+            colors: {
+                "airbnb-theme-color": "#FF385C",
+                "airbnb-light-black": "#222222",
+                "airbnb-light-gray": "#717171",
+            },
+
+            gridTemplateRows: {
+                "new-listing": "10vh 80vh 10vh",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
 export default config
